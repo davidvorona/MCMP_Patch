0ead7dcb041182e766db7da119140c7a20dccda2
David Vorona <davidavorona@gmail.com>
2024-10-21 09:08:03 -0700

    Update version to 1.0.9

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -6,6 +6,6 @@ namespace MCMP_Patch
     {
         public const string PLUGIN_GUID = "Vijuhas.MCMP_Patch";
         public const string PLUGIN_NAME = "MCMP_Patch";
-        public const string PLUGIN_VERSION = "1.0.8";
+        public const string PLUGIN_VERSION = "1.0.9";
     }
 }
